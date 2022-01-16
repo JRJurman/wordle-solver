a2e9aad14b4f70d6d333f9a7d149a5bcabddc543
JRJurman <j.r.jurman@gmail.com>
2022-01-16 02:53:33 -0500

    interactive mode

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # wordle-solver
 Wordle Solver
 
+Special thanks to [@chordbug](https://twitter.com/chordbug) and their [hello wordl](https://foldr.moe/hello-wordl/#), which was used extensively for testing.
+
 Word List from https://en.lexipedia.org/
 * 5 to 5 letters, minimum frequency 100, and not only wikitionary words
 
 ## How to use
-In the non-interactive mode (only option right now), fill in the information in `tester.js`, and then run `node tester.js`.
+**Depends on Node 17**
+Run `node tester.js`, and follow the prompts:
+* First you'll always be given two answers, one based on the number of unique letters (good for initial guesses) and one based on popularity (good for final guess).
+* Next, it'll ask you to enter any letters that are not in the word, you should type this with no punctuation (e.g. if 'f', 'i', and 'r', were not part of the word, write it as `fir`)
+* Next, it'll ask you to enter any letters that are part of the word, you should type this the same as above - be sure to include any letters that were correct but in the wrong space
+* Next, it'll ask you about each slot that has not already been resolved - type either `not _`, `is _` or simply hit the enter key.
+* After that it loops, kill the program by hitting CTRL+C
 
 ## Files
 
--- tester.js
+++ tester.js
@@ -1,18 +1,59 @@
 const LexipediaProcessor = require('./lexipediaProcessor')
 
-/* empty state
+const readlinePromises = require('readline/promises');
+const cli = readlinePromises.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const information = ({
 	invalidLetters: [],
 	requiredLetters: [],
-	slots: [{}, {}, {}, {}, {}]
+	slots: [
+		{is: undefined, not: []},
+		{is: undefined, not: []},
+		{is: undefined, not: []},
+		{is: undefined, not: []},
+		{is: undefined, not: []}
+	]
 })
-*/
 
-const information = ({
-	invalidLetters: ['f', 'i', 't', 'y', 'a', 'o', 'l'],
-	requiredLetters: ['r', 's', 'e'],
-	slots: [{not: ['r']}, {not: ['e']}, {not: ['r']}, {is: 'e'}, {is: 's'}]
-})
+const runTester = async () => {
 
-console.log('calculate next best answer: ', LexipediaProcessor.nextBestAnswer(information))
-console.log('calculate next best hint: ', LexipediaProcessor.nextBestHint(information))
+	while (true) {
+		// post possible solutions
+		const nextBestHint = LexipediaProcessor.nextBestHint(information) || {}
+		console.log('next best hint:', `"${nextBestHint.w}"`, nextBestHint.p)
+
+		const nextBestAnswer = LexipediaProcessor.nextBestAnswer(information) || {}
+		console.log('next best answer:', `"${nextBestAnswer.w}"`, nextBestAnswer.p)
+
+		// ask questions about the results
+		const invalidLetters = await cli.question(`invalid letters? ${information.invalidLetters}: `)
+		information.invalidLetters.push(...invalidLetters.split(''))
+
+		const requiredLetters = await cli.question(`required letters? ${information.requiredLetters}: `)
+		information.requiredLetters.push(...requiredLetters.split(''))
+
+		const updateSlot = async (index) => {
+			const slot = information.slots[index]
+			if (!slot.is) {
+				const slotUpdate = await cli.question(`slot ${index + 1} update? `)
+				if (slotUpdate === '') return;
+				const [command, letter] = slotUpdate.split(' ')
+				if (command === 'not') { slot.not.push(letter) }
+				if (command === 'is') { slot.is = letter }
+			}
+		}
+
+		await updateSlot(0)
+		await updateSlot(1)
+		await updateSlot(2)
+		await updateSlot(3)
+		await updateSlot(4)
+
+	}
+
+}
+
+runTester()
